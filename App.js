4270c05c0b008fdea06e98690fba8deddb62dd75
sureshdubbal91 <141761386+sureshdubbal91@users.noreply.github.com>
2023-12-18 22:54:20 +0530

    new files

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {
+    id :"heading"
+}, "HELLO WORLD REACT");
+        const root = ReactDOM.createRoot (document.getElementById ("root"));
+        root.render(heading);
